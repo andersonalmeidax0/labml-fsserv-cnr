190112f034915f93230f7edc7b8c5c30e5f4f292
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-29 19:36:25 -0300

    Update _build.sh

--- _build.sh
+++ _build.sh
@@ -1,3 +1,4 @@
-sudo docker build --no-cache -t="labml-fsserv-img" -f ./Dockerfile
+#o ultimo parametro eh o contexto de COPY, etc
+sudo docker build --no-cache -t="labml-fsserv-img" -f ./Dockerfile .
 sudp docker push labml-fsserv-img
 
