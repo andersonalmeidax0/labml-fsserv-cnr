abd1aa79a970dc37775e557de2844e74a0f1a4de
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-29 18:57:22 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -1,1 +1,3 @@
-docker run -d --name webdav -p 80:80 -v "$PWD":/var/www visity/webdav
+#parameter -d =>   
+#parameter -p HOSTPORT:CONTAINER_PORT
+sudo docker run -d --name labml-fsserv-cnr -p 8090:80 -v "$PWD":/var/www labml-fsserv-img
