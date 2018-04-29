960e3c84b81ece0445e93b05751737a3530c17b0
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-29 20:45:29 -0300

    Update _start-debug.sh

--- _start-debug.sh
+++ _start-debug.sh
@@ -1,1 +1,4 @@
 #DEBUG: tirar -d e colocar -t -i com /bin/bash no final
+#parameter -d =>   detached mode, ou seja como deamon
+#parameter -p HOSTPORT:CONTAINER_PORT
+sudo docker run -t -i --name labml-fsserv-cnr -p 8090:80 -v "$PWD":/var/www labml-fsserv-img /bin/bash
