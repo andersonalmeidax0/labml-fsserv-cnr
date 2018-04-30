2c531dae6168a9014430e7a059deab31e3272a31
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-29 23:42:59 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -1,3 +1,3 @@
 #parameter -d =>   detached mode, ou seja como deamon
 #parameter -p HOSTPORT:CONTAINER_PORT
-sudo docker run -d --name labml-fsserv-cnr -p 8090:80 -v "$PWD":/var/www labml-fsserv-img
+sudo docker run -d --name labml-fsserv-cnr -p 8090:80  -e "WEBDAV_USERNAME=user1" -e "WEBDAV_PASSWORD=user1" -v "$PWD":/var/www labml-fsserv-img
