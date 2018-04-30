58e3c0a7ae21b9231bca859a9af83c4d86178304
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-29 23:44:56 -0300

    Update _start-debug.sh

--- _start-debug.sh
+++ _start-debug.sh
@@ -1,4 +1,7 @@
 #DEBUG: tirar -d e colocar -t -i com /bin/bash no final
 #parameter -d =>   detached mode, ou seja como deamon
 #parameter -p HOSTPORT:CONTAINER_PORT
-sudo docker run -t -i --name labml-fsserv-cnr -p 8090:80 -v "$PWD":/var/www labml-fsserv-img /bin/bash
+#export WEBDAV_USERNAME=user1
+#export WEBDAV_PASSWORD=user1
+#sudo docker run -t -i --name labml-fsserv-cnr -p 8090:80 -e WEBDAV_USERNAME -e WEBDAV_PASSWORD  -v "$PWD":/var/www labml-fsserv-img /bin/bash
+sudo docker run -t -i --name labml-fsserv-cnr -p 8090:80 -e "WEBDAV_USERNAME=user1234" -e "WEBDAV_PASSWORD=user1234"  -v "$PWD":/var/www labml-fsserv-img /bin/bash
