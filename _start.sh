e6036cdac0e9019a07c1cc34be0e3065f0a4854f
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-04-30 00:04:16 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -1,3 +1,5 @@
 #parameter -d =>   detached mode, ou seja como deamon
 #parameter -p HOSTPORT:CONTAINER_PORT
-sudo docker run -d --name labml-fsserv-cnr -p 8090:80  -e "WEBDAV_USERNAME=user1" -e "WEBDAV_PASSWORD=user1" -v "$PWD":/var/www labml-fsserv-img
+#tem que criar diretorio www na CURDIR e dar chmod +777
+#sudo docker run -d --name labml-fsserv-cnr -p 8090:80  -e "WEBDAV_USERNAME=user1234" -e "WEBDAV_PASSWORD=user1234" -v "$PWD":/var/www labml-fsserv-img
+sudo docker run -d --name labml-fsserv-cnr -p 8090:80   -v "$PWD/www":/var/www labml-fsserv-img
