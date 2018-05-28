a838a36ada8a1e940ec24fc634da18ce1824a318
andersonalmeidax0 <36134545+andersonalmeidax0@users.noreply.github.com>
2018-05-28 15:53:33 -0300

    Update _start.sh

--- _start.sh
+++ _start.sh
@@ -2,4 +2,4 @@
 #parameter -p HOSTPORT:CONTAINER_PORT
 #tem que criar diretorio www na CURDIR e dar chmod +777
 #sudo docker run -d --name labml-fsserv-cnr -p 8090:80  -e "WEBDAV_USERNAME=user1234" -e "WEBDAV_PASSWORD=user1234" -v "$PWD":/var/www labml-fsserv-img
-sudo docker run -d --name labml-fsserv-cnr -p 8090:80   -v /var/apps/labml:/var/www labml-fsserv-img
+sudo docker run -d --name labml-fsserv-cnr -p 8090:80 --restart=unless-stopped  -v /var/apps/labml:/var/www labml-fsserv-img
